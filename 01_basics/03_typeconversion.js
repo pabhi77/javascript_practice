8c57fdabb576fa0c1f023fa9fe10df1c62a8a6a4
pabhi77 <86058581+pabhi77@users.noreply.github.com>
2024-09-19 17:39:49 +0000

    ccccc

---
+++ 01_basics/03_typeconversion.js
@@ -0,0 +1,62 @@
+let string="abhi"
+let string2=true
+let convertNumber=Number(string)
+console.log(typeof convertNumber)
+console.log(convertNumber)
+let convertNum2=Number(string2)
+console.log(typeof convertNum2)
+console.log(convertNum2)
+
+//"33"=>33  "33abs"=>NaN not a number   true=>1  false=>0
+
+
+let isLoggedIn = "abhi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);// -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);//hello hitesh
+
+// console.log("1" + 2);12
+// console.log(1 + "2");12
+// console.log("1" + 2 + 2);122
+// console.log(1 + 2 + "2");32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);1
+// console.log(+"");0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);//101
