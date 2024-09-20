416c3a0e6c92e38cd4f6ccf55d44c1809b7c7877
pabhi77 <86058581+pabhi77@users.noreply.github.com>
2024-09-20 05:59:25 +0000

    comitted

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -14,3 +14,7 @@ console.log(typeof myArray)
 // var variables can be updated and re-declared within its scope; let variables can be updated but not re-declared; const variables can neither be updated nor re-declared.
 // They are all hoisted to the top of their scope. But while var variables are initialized with undefined, let and const variables are not initialized.
 // While var and let can be declared without being initialized, const must be initialized during declaration.
+
+
+// let properties
+//block scoped 
