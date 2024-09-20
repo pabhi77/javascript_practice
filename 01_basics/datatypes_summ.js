0f3649bd7ba2a6c20300bfd560cc561f06923941
pabhi77 <86058581+pabhi77@users.noreply.github.com>
2024-09-20 06:44:45 +0000

    comiited with myfunc correction

--- 01_basics/datatypes_summ.js
+++ 01_basics/datatypes_summ.js
@@ -13,21 +13,21 @@ let userEmail;//undefined
 const myVar=Symbol('123')
 const myVar1=Symbol('123')//both will have different symbol even though it is inputted same
 const num=23243546757568n
-console.log(typeof num)
-console.log(myVar==myVar1)//returns false
+// console.log(typeof num)
+// console.log(myVar==myVar1)//returns false
 
 
 //non primitive
 const myArr=[1,'hii',true,0.12,null]
-console.log(myArr)
+//console.log(myArr)
 
 let myObj={
     name:"Abhi",
     score:34
 }
-console.log(myObj)
+//console.log(myObj)
 
 const myfunc=function(){
     console.log("hiii")
 }
-console.log(myfunc())
+ myfunc()
